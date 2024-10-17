dd7ec4b2330facf63ba6b29f6c893f6fc83ca398
Muskan Akhtar <muskanakhter2627@gmail.com>
2024-10-17 20:14:21 +0500

    Initial commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className='bg-gray-800 text-white text-center p-4'>
+        2024 Muskan Akhtar All rights reserved.
+        </div>
+    </footer>
+  )
+}
+export default Footer;
